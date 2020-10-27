433e39f286d374a742398a8f0328c3cd475d6b59
artemgur <artemgur01@gmail.com>
2020-10-27 14:07:12 +0300

    Added fields for photos to articles and users

--- sem.sql
+++ sem.sql
@@ -4,23 +4,25 @@ CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     login VARCHAR(20) NOT NULL UNIQUE,--store hash instead of actual value?
     password VARCHAR(40) NOT NULL, --store hash instead of actual value?
-    email TEXT NOT NULL UNIQUE
+    email TEXT NOT NULL UNIQUE,
+    photo TEXT --path to photo
 );
 
-CREATE TABLE article(
+CREATE TABLE articles(
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL, --TODO should be unique?
     date TIMESTAMP,
-    text TEXT NOT NULL--path to markdown file, not text itself
+    text TEXT NOT NULL,--path to markdown file, not text itself
+    photo TEXT --path to photo
 );
 
 CREATE TABLE tags(
     article_id INTEGER NOT NULL,
     tag TAG NOT NULL,
-    FOREIGN KEY (article_id) REFERENCES article(id) ON DELETE CASCADE
+    FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE
 );
 --We will search in both directions
-CREATE INDEX tags_article_index ON tags(article_id);
+CREATE INDEX tags_articles_index ON tags(article_id);
 CREATE INDEX tags_tag_index ON tags(tag);
 
 CREATE TABLE debates( --debates header can be treated as a comment
@@ -53,6 +55,6 @@ CREATE TABLE favourite_articles(
     user_id INTEGER,
     article_id INTEGER,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (article_id) REFERENCES article(id) ON DELETE CASCADE
+    FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE
 );
 CREATE INDEX favourite_articles_user_index ON favourite_articles(user_id);
