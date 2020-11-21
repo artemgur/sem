681b23f78d6aef48dc91d350addc4efea70790e1
artemgur <artemgur01@gmail.com>
2020-11-21 20:29:53 +0300

    Changes

---
+++ Sem/ResourcesPaths.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Database;
+
+namespace Sem
+{
+	public static class ResourcesPaths
+	{
+		public static string GetPersonPhotoPath(Entity e)
+		{
+			//e.Values.TryGetValue("photo", out var s);
+			var s = e.Values["photo"];
+			return s is DBNull ? "imgs/dog.png" : $"Resources/PeoplePhotos/{s}.jpg";
+		}
+
+		public static string GetArticleImagePath(Entity e)
+		{
+			//e.Values.TryGetValue("photo", out var s);
+			var s = e.Values["photo"];
+			return s is DBNull ? "imgs/dog.png" : $"Resources/ArticleImages/{s}.jpg";
+		}
+
+		public static string GetUserImagesPath(Entity e)
+		{
+			//e.Values.TryGetValue("photo", out var s);
+			var s = e.Values["photo"];
+			return s is DBNull ? "imgs/dog.png" : $"Resources/UserImages/{s}.jpg";
+		}
+	}
+}
---
+++ Sem/RestrictedPageModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sem
+{
+	public abstract class RestrictedPageModel : PageModel
+	{
+		public Entity User;
+		
+		public async Task<IActionResult> OnGet()
+		{
+			User = await Authentication.GetUserInfoOrRedirect(HttpContext);
+			if (User == null)
+				return Redirect("login");
+			return null;
+		}
+	}
+}
